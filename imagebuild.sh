62e5d2c8bcb6006b2f6413f3c582ca27d9572687
appbank4 <appbank22@gmail.com>
2023-01-02 12:29:33 -0500

    docker file updated
    Signed-off-by: appbank4 <appbank22@gmail.com>

---
+++ imagebuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#script to automate docker image build
+#Author: Engr. Adekunle Adeyemi
+
+today=`date`
+user=`whoami`
+dockeruser=yourcase
+password=yourcase
+dockerrepo=yourcase
+versions=( 18.04 20.04 21.10 16.04 22.04 )
+for i in ${versions[@]}; do
+docker build -t $dockeruser/$dockerrepo:$i --build-arg versions=groovy .
+docker login -u $dockeruser -p $password
+docker push $dockeruser/$dockerrepo:$i
+echo "$user is running a docker container with etechteam2/etecapp:$i on $today"
+sleep 5
+echo " Images have been uploaded to your dockerhub repository !! "
+done
